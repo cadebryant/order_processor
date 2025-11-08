6873323c33d27987be8829a295e31a3d884c1a11
cadebryant <cadehbryant@gmail.com>
2025-11-08 01:26:00 -0500

    initial commit

---
+++ OrderProcessorTests/Test1.cs
@@ -0,0 +1,13 @@
+﻿using LegacyOrderProcessor;
+
+namespace OrderProcessorTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
